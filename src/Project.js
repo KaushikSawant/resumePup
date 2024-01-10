5c89e8554ed8a31d6c611fa1b92e09ed68c3db2a
unknown <kaushiksawant93@gmail.com>
2024-01-10 08:17:43 +0530

    experience updated

--- src/Project.js
+++ src/Project.js
@@ -6,26 +6,24 @@ function Project(){
         <div className="row justify-content-end">
             <div className="col-12 col-md-8 card bgProject">
                 <div className="card-body">
-                    <h3>Work Experience</h3>
-                    <h4>Ugam Solutions<img src = {`https://www.ugamsolutions.com/Content/images/logo.png`} className="mastercard" /></h4>
-                    
-                    <p>
-                        E Learning is a web application which provides an interface through which users can learn new programming
-                        languages by reading detailed tutorials and coding alongside. The learners can also test their knowledge
-                        after completing a module. New courses and the questions for the MCQ tests can be added by the admin.
-                        There is also a problem section available, where the user has to match the exact output expected by the problem.
-                    </p>    
-                </div>
-
-                <div className="card-body">
+                <h3>Work Experience</h3>
                     <h4>Finicity a Mastercard Company <img src={img.ma} className="mastercard" /></h4>
-                    <h5>Technologies used: Java MVC</h5>
                     <p>
-                        E Learning is a web application which provides an interface through which users can learn new programming
-                        languages by reading detailed tutorials and coding alongside. The learners can also test their knowledge
-                        after completing a module. New courses and the questions for the MCQ tests can be added by the admin.
-                        There is also a problem section available, where the user has to match the exact output expected by the problem.
+                        As a Software Engineer, I played a pivotal role in the Open Banking division, where my responsibilities included writing highly efficient code using Puppeteer, JavaScript, and Typescript.
+                        My primary role is to leverage the Puppeteer framework to automate browser actions.
+                        Additionally, I played a pivotal role in maintaining the overall repository health by resolving SonarQube issues, addressing code complexities, and implementing best practices.
+                        My responsibilities also extended to upgrading Puppeteer and TypeScript versions, necessitating code modifications to align with the latest updates.
                     </p> 
+                </div>
+                <div className="card-body">
+                    
+                    <h4>Ugam Solutions<img src = {`https://www.ugamsolutions.com/Content/images/logo.png`} className="mastercard" /></h4>
+                    <p>
+                        As a Software Engineer, I played a pivotal role in the Open Banking division, where my responsibilities included writing highly efficient code using Puppeteer, JavaScript, and Typescript.
+                        My primary role is to leverage the Puppeteer framework to automate browser actions.
+                        Additionally, I played a pivotal role in maintaining the overall repository health by resolving SonarQube issues, addressing code complexities, and implementing best practices.
+                        My responsibilities also extended to upgrading Puppeteer and TypeScript versions, necessitating code modifications to align with the latest updates.
+                    </p>    
                 </div>
             </div>                
         </div>    
